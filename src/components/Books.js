6678827adc71f9f99fab3d6d9f38913e083d770f
fikerte <tofikertesfaye@gmail.com>
2021-11-19 14:15:34 +0300

    Add react component for adding new books

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,14 +1,7 @@
 import React from 'react';
+import AddBooks from './AddBook';
 
 const Books = () => (
-  <div>
-    <h3>ADD NEW BOOK</h3>
-    <input type="text" placeholder="Book title" />
-    <select name="Category">
-      <option value="">Category</option>
-    </select>
-    <button type="submit">ADD BOOK</button>
-  </div>
-
+  <AddBooks />
 );
 export default Books;
